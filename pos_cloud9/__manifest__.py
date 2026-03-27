e78779c893e54441897a5348b17ac788a242b831
CTSSAW <103968001+CTSSAW@users.noreply.github.com>
2026-03-27 10:35:50 -0400

    Update summary for Cloud9 POS Payment Gateway

--- pos_cloud9/__manifest__.py
+++ pos_cloud9/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Cloud9 POS Payment Gateway (Odoo Community Compatible)',
     'version': '18.0.1.0.0',
     'category': 'Sales/Point of Sale',
-    'summary': 'Our Online Payment Gateway, bring your processor or we will help you pick a new one. Compatible with most processors, Elavon, TSYS, Heartland, Global Payments, Nuvei, and Worldpay to name a few (Community & Enterprise)  ',
+    'summary': 'POS Payment Gateway — Bring yours processor or we’ll help you choose (Community & Enterprise). Elavon, TSYS, Global, Nuvei, and more',
     'description': 'Cloud9 Payment Gateway integration for Odoo POS. Accept payments, run tabs, and support multiple processors. Fully compatible with Odoo Community and Enterprise editions. Ideal for clubs, restaurants, and multi-terminal environments.',
     'author': 'C9PG',
     'website': 'https://c9pg.com',
